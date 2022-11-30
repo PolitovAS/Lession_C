23ab237ab880cf170e5dfe31a9df88b992e9ec1d
Alexandr Politov <razreztv@gmail.com>
2022-11-30 23:26:47 +0500

    New repo

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine() ?? "";
+
+if(username.ToLower() == "alex")
+{
+    Console.WriteLine("Ура, это же Alex!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

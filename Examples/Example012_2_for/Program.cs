3e25f6b660139825519a718c1a8af6cc32d62146
Alexandr Politov <razreztv@gmail.com>
2022-12-07 23:47:33 +0500

    Add Examples 012 and 012_2

---
+++ Examples/Example012_2_for/Program.cs
@@ -0,0 +1,126 @@
+﻿// Метод 4-го вида с циклом for
+string Method4(int count, string c)
+{
+    string result = String.Empty; // резалт будет пустой строкой
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }     
+    return result;
+}
+
+string res = Method4(10, "Ha");
+Console.WriteLine(res);
+
+// Цикл в цикле: таблица умножения
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+/* Дан текст. В нём нужно все пробелы заменить чёрточками, маленькие буквы «к»
+заменить большими «К», а большие «С» заменить на маленькие «с».*/
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+/*
+string s = "qwerty"
+            012345
+s[3] -> r
+*/
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '_');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+
+//Упорядочить массив. алгоритм сортировки методом выбора
+/*int[] arr = {1, 3, 4, 6, 7, 8, 5, 2, 5, 7};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);*/
+
+//Упорядочить массив от большего к меньшему
+int[] arr = {1, 3, 4, 6, 7, 8, 5, 2, 5, 7};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
---
+++ Examples/Example012_Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿// Вид 1 ничего не возвращает, и ничего не принимает
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+
+// Вид 2 ничего не возвращает, но принимает аргументы
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+void Method2_1(string msg, int count) // именованные аргументы
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++; // Инкримент(увеличение), Декримент(уменьшение)
+    }
+}
+Method2_1("Текст", 4);
+Method2_1(count: 3, msg: "Текст"); // можно не по порядку, если именованные
+
+// Вид 3 что-то возвращают, но ничего не принимают
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4 что-то принимают и что-то возвращают
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty; // резалт будет пустой строкой
+
+    while(i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "Ha");
+Console.WriteLine(res);

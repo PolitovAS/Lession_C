aa6f9e81ecac63e4f9560c449c8811dbcfd5f5b4
Alexandr Politov <razreztv@gmail.com>
2022-12-14 22:53:54 +0500

    Add Examples 13_1, 13_2, 13_3 (TwoDimensionalArray, PaintOver, RecursionAlgorithm)

---
+++ Examples/Example013_1_TwoDimensionalArray/Program.cs
@@ -0,0 +1,38 @@
+﻿/*string[,] table = new string[2, 5];
+
+table[1,2] = "Слово";
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"-{table[i, j]}-");
+    }
+}*/
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Examples/Example013_3_RecursionAlgorithm/Program.cs
@@ -0,0 +1,28 @@
+﻿//Факториал
+/*double Factorial (int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+
+//Фибаначи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double F(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return F(n-1) + F(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {F(i)}");
+}
